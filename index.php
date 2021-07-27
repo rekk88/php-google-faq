8c5bd091a56cdf8943589d6d9e5da6fd47de3003
maicol <maicol.matteoli@gmail.com>
2021-07-27 15:02:00 +0200

    init + header

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php google faq</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;1,100;1,300;1,400&display=swap" rel="stylesheet"> 
+</head>
+<body>
+    <header>
+        <!-- header top -->
+        <div class="top">
+            <div class="logo_wrap">
+                <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="logo">
+                <span class="">Privacy e termini</span>
+            </div>
+            <div class="account_wrap">
+                <svg class="gb_Ve" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>
+                <!-- <img class="gb_Ca gbii" src="https://lh3.googleusercontent.com/ogw/ADea4I583njUoOOaUFQ53Aqdftv6Jy8o8a14HW1OEtUi=s32-c-mo" srcset="https://lh3.googleusercontent.com/ogw/ADea4I583njUoOOaUFQ53Aqdftv6Jy8o8a14HW1OEtUi=s32-c-mo 1x, https://lh3.googleusercontent.com/ogw/ADea4I583njUoOOaUFQ53Aqdftv6Jy8o8a14HW1OEtUi=s64-c-mo 2x" alt="" aria-hidden="true" data-noaft=""> -->
+                <img src="img/user.png" alt="user"/>
+            </div>
+        </div>
+       <!-- fine header top -->
+       <!-- header bottom -->
+       <div class="bottom">
+           header bottom
+       </div>
+       <!--fine header bottom -->
+
+    </header>
+    <main></main>
+</body>
+</html>
